37dd7a91986d25f306784961475c82d27b7a09d8
zerosnake0 <zerosnake0@gmail.com>
2019-11-06 00:51:41 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"time"
+	"strconv"
+	"net/http/httputil"
+	"bytes"
+)
+
+var (
+	port    int
+	secure  bool
+	pemPath string
+	keyPath string
+)
+
+func init() {
+	flag.IntVar(&port, "port", 9000, "port")
+	flag.BoolVar(&secure, "secure", false, "use https")
+	flag.StringVar(&pemPath, "pem", "server.pem", "path to pem file")
+	flag.StringVar(&keyPath, "key", "server.key", "path to key file")
+	flag.Parse()
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
+
+func handleConnect(w http.ResponseWriter, r *http.Request) {
+	proxyURL, err := http.ProxyFromEnvironment(r)
+	if err != nil {
+		log.Println("unable to get proxy url", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	var host string
+	if proxyURL == nil {
+		host = r.URL.Host
+	} else {
+		host = proxyURL.Host
+	}
+	dstConn, err := net.DialTimeout("tcp", host, 10*time.Second)
+	if err != nil {
+		log.Println("unable to dial", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	if proxyURL != nil {
+		b, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			log.Println("unable to dump request", err)
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			return
+		}
+		if _, err := io.Copy(dstConn, bytes.NewReader(b)); err != nil {
+			log.Println("unable to send connection", err)
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		log.Println("Hijacking not supported")
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	srcConn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	go transfer(dstConn, srcConn)
+	go transfer(srcConn, dstConn)
+}
+
+func handleHTTP(w http.ResponseWriter, req *http.Request) {
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":" + strconv.Itoa(port),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodConnect {
+				handleConnect(w, r)
+			} else {
+				handleHTTP(w, r)
+			}
+		}),
+		// Disable HTTP/2.
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	log.Println("serving...")
+	if secure {
+		log.Fatal(server.ListenAndServeTLS(pemPath, keyPath))
+	} else {
+		log.Fatal(server.ListenAndServe())
+	}
+}
